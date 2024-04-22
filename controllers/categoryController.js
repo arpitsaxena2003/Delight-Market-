f49e246829a8f8e7d06fb77ff675155d10bb72a0
Arpit Saxena <arpitsaxena2368@gmail.com>
2024-04-23 02:22:21 +0530

    categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -57,5 +57,43 @@ export const updateCategoryController = async (req, res) => {
     });
   }
 };
+//get all cat
+export const categoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "All categories List",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting all categories",
+    });
+  }
+};
+
+// single category
+export const singleCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: "Get SIngle Category SUccessfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error While getting Single Category",
+    });
+  }
+};
+
 
     
