060998d8975a416585f23ae879e843e5c00897dd
Arpit Saxena <arpitsaxena2368@gmail.com>
2024-04-26 02:43:58 +0530

    product details

---
+++ models/productModel.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    }
+    
