e0462abc0caf8b3d2f53289a987dfc1eef436344
Arpit Saxena <arpitsaxena2368@gmail.com>
2024-04-26 02:45:44 +0530

    final

--- models/productModel.js
+++ models/productModel.js
@@ -13,5 +13,29 @@ const productSchema = new mongoose.Schema(
     description: {
       type: String,
       required: true,
-    }
-    
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: mongoose.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      data: Buffer,
+      contentType: String,
+    },
+    shipping: {
+      type: Boolean,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Products", productSchema);
